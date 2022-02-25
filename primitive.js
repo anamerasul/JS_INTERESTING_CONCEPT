89cb34b526ad847f0030dea04099bf81145fab14
anamerasul <anamerasul@gmail.com>
2022-02-25 18:04:27 +0600

    restart

---
+++ primitive.js
@@ -0,0 +1,37 @@
+// data types
+
+// primitive data types
+// 1 Number
+// 2.String
+// 3.bolean
+// 4 Undefined
+// 5 Null
+
+// non primitive data types
+// 6 Object
+//arry
+// 7.symbol
+
+// const a =[2,4,6,8,10]
+// const a ={name:'h'}
+
+// console.log(typeof a)
+
+let a ='hello'
+let b=a;
+console.log(a,b)
+a='gello';
+console.log(a,b)
+
+const x ={job:'web developer '}
+
+const y =x;
+
+console.log(x,y)
+
+// x.job='front end'
+y.job='front end'
+
+console.log(x,y)
+
+

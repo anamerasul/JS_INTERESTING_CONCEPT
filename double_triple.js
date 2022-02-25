6177df63de7813d13199e7b0e2a0c6ce225d38c9
anamerasul <anamerasul@gmail.com>
2022-02-25 19:08:36 +0600

    double equal (==) vs triple equal (===), implicit conversion

---
+++ double_triple.js
@@ -0,0 +1,32 @@
+const first='0';
+const second=false;
+
+if(first==second){
+        console.log('condition true')
+}
+
+else{
+        console.log('condition false')
+}
+
+// implicit conversion
+
+// more comparison
+const a ={name:'ali'}
+const b ={name:'ali'}
+
+if(a==b){
+        console.log('both are sme')
+}
+else{
+        console.log('they are not same')
+}
+const x =[]
+const z =[]
+
+if(x==z){
+        console.log('both are sme')
+}
+else{
+        console.log('they are not same')
+}
---
+++ undifine_null.js
@@ -0,0 +1,69 @@
+/* 
+undefine
+variable not assign
+function  without returtn
+// just write return but without return any thing
+
+parameter doest pass
+
+property doesn axixt an object
+access array element out of range
+accessing delete element en array;
+
+// explicity set undefined
+
+*/
+
+let first;
+
+// console.log(first);
+function second(x ,y) {
+
+        const sum =x+y
+}
+
+const result = second(3,4)
+
+console.log(result)
+
+function add(a,b){
+        const sum=a+b;
+
+        if(b<10){
+                return; 
+        }
+
+
+        return;
+       const fun =a*4 
+       return fun
+}
+
+console.log(add(2,7))
+
+
+function double(a,b){
+        const result = a*b;
+
+        console.log(b)
+        return result
+}
+
+double(23)
+
+const fifth ={name:'fakir',age:15,location:'dhaka'}
+
+console.log(fifth.phone)
+
+const sixth=[54,34,41,56];
+
+console.log(sixth[6])
+delete sixth[2];
+
+console.log(sixth)
+console.log(sixth[2])
+
+
+const seven=undefined;
+
+const myobject ={name:'samad',profession:null}

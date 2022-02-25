c81c6271b0aa869dddb1656104d739adf179c825
anamerasul <anamerasul@gmail.com>
2022-02-25 18:18:38 +0600

    Different Truthy and Falsy values in JavaScript

---
+++ true_false.js
@@ -0,0 +1,35 @@
+// false
+
+// falsy
+// false
+//0
+//empty string
+//undifined
+//null
+
+// NaN
+
+//------
+
+// truthy
+// true 
+// any number
+//any string including single white space '0'
+
+//empty array 
+// empty object
+
+// let x=null;
+
+// let x=parseInt('hello')
+let x=[2,4,6];
+
+console.log('value of' ,x )
+
+if(x){
+        console.log('variable true')
+}
+
+else{
+        console.log('variable false')
+}
